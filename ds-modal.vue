3db2e18da85e9db25441e7c00e9fb47f958b2b91
Jonathan Doron <yonidn7@gmail.com>
2020-02-20 16:21:18 +0200

    Create ds-modal.vue

---
+++ ds-modal.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="cd-modal-overlay"
+         :style="getComputedStyles">
+       <div class="modal-background-click"
+            @click="clickOutsideModal">
+           <div class="modal-box" @click.stop>
+               <slot></slot>
+           </div>
+       </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'cdModal',
+		props: ['config'],
+        components: {
+        
+        },
+        data: function() {
+        	return {
+				width: 600,
+				height: 300
+            }
+        },
+		computed: {
+			getComputedStyles() {
+				return [
+					{'--cd-modal-width': this.width + 'px'},
+					{'--cd-modal-height': this.height + 'px'},
+				];
+			},
+		},
+        methods: {
+			setStyles() {
+				if (this.config) {
+					if (this.config.width) {
+						this.width = this.config.width;
+					}
+					if (this.config.height) {
+						this.height = this.config.height;
+					}
+				}
+			},
+            clickOutsideModal() {
+				this.$emit('closeModal');
+            },
+            escapeKeyListener(evt) {
+				if (evt.key.toLowerCase() === 'escape') {
+					this.clickOutsideModal();
+                }
+            },
+            appendEscapeListener() {
+				document.addEventListener('keyup', this.escapeKeyListener);
+            },
+            ditachEscapeListener() {
+				document.removeEventListener('keyup', this.escapeKeyListener);
+            }
+            
+        },
+        created() {
+        	this.appendEscapeListener();
+			this.setStyles();
+        },
+        destroyed() {
+        	this.ditachEscapeListener();
+        }
+    }
+</script>
+
+<style scoped
+       lang="scss">
+    @import "./cd-modal";
+</style>
